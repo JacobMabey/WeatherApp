64563390f5b7737651bd6157cfcfe37ef56a191b
OliverQueenEpicMoments <jguerradominguez@student.neumont.edu>
2022-11-14 10:16:58 -0700

    Got the UI/Design down, ignore the images and thext they are simply placeholders

--- Form1.cs
+++ Form1.cs
@@ -6,5 +6,42 @@ namespace WeatherApp
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        // Most of these click events were made on accident and likely wont be used
     }
 }
