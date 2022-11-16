e9b3802337448b4398ca7b4424d03c94f3419726
JacobMabey <mabey.jacob@gmail.com>
2022-11-16 08:34:59 -0700

    Moved ForecastDay class to the weather receiver class

--- WeatherReceiver.cs
+++ WeatherReceiver.cs
@@ -339,4 +339,26 @@ namespace WeatherApp
     {
         IMPERIAL, METRIC
     }
+
+
+
+
+
+    public class ForecastDay
+    {
+        public string Weather { get; private set; }
+        public string WeatherDescription { get; private set; }
+        public double Temperature { get; private set; }
+        public double TemperatureLow { get; private set; }
+        public double TemperatureHigh { get; private set; }
+
+        public ForecastDay(string weather, string weatherDesc, double temp, double tempLow, double tempHigh)
+        {
+            Weather = weather;
+            WeatherDescription = weatherDesc;
+            Temperature = temp;
+            TemperatureLow = tempLow;
+            TemperatureHigh = tempHigh;
+        }
+    }
 }
